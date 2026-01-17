25fcc93e6ce0bf4a097f6bc57a7ac26e1dd52135
rasheedbshaik <rasheedbasha.2004@gmail.com>
2026-01-17 12:20:57 +0530

    update server.js to for render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors'; // 1. Import cors
+import cors from 'cors';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
 
@@ -9,19 +9,27 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// 2. Enable CORS
+const allowedOrigin = process.env.CLIENT_URL || "*";
+
 app.use(cors({
-    origin: "http://localhost:5173"||"https://1s8dbpgj-5173.inc1.devtunnels.ms", 
-    // Allow your React/Vite dev server
+    origin: allowedOrigin,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
 
 app.use(express.json());
-
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
-    connectDB();
-    console.log('Server started at http://localhost:' + PORT);
-});
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Failed to connect to DB", error);
+        process.exit(1);
+    }
+};
+
+startServer();
