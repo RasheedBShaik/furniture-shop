e33e98126f1cd47af5f4af2e1fc665286e600103
Rasheed Shaik <161990813+RasheedBShaik@users.noreply.github.com>
2026-01-20 12:11:47 +0530

    Refactor About page layout and content

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -6,7 +6,6 @@ const About = () => {
     <div className="max-w-7xl mx-auto my-6 px-4 md:px-0">
       {/* Banner / Header */}
       <div className="bg-[url(/images/shop-hero.png)] bg-cover bg-center flex items-center justify-center flex-col h-80 py-12 mb-10 text-center rounded-sm">
-        <img src="/icons/logo.png" alt="Logo" className="mb-2" />
         <h1 className="text-4xl font-bold text-gray-800 not-italic">About</h1>
         <p className="mt-2 not-italic">
           <Link to="/" className="hover:text-[#B88E2F] text-lg font-semibold">Home</Link> &gt;{" "}
@@ -124,3 +123,4 @@ const About = () => {
 
 
 export default About
+
